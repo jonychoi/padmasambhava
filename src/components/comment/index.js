3dee202465e72810f05197e98ae5e309d77161ca
Jonathan Choi <thesky7@korea.ac.kr>
2022-10-27 00:27:54 +0900

    update

--- src/components/comment/index.js
+++ src/components/comment/index.js
@@ -1,14 +1,13 @@
 import Giscus from '@giscus/react';
-require('dotenv').config()
 
 export const Comment = () => {
   return (
     <Giscus
       id="comments"
       repo="jonychoi/padmasambhava"
-      repoId={process.env.REPOID}
+      repoId="R_kgDOIUASlg"
       category="Q&A"
-      categoryId={process.env.CATEGORYID}
+      categoryId="DIC_kwDOIUASls4CSNOx"
       mapping="pathname"
       term="Welcome to @giscus/react component!"
       reactionsEnabled="1"
@@ -22,3 +21,19 @@ export const Comment = () => {
 };
 
 export default Comment;
+
+// <script src="https://giscus.app/client.js"
+//         data-repo="jonychoi/padmasambhava"
+//         data-repo-id="R_kgDOIUASlg"
+//         data-category="Q&A"
+//         data-category-id="DIC_kwDOIUASls4CSNOx"
+//         data-mapping="pathname"
+//         data-strict="0"
+//         data-reactions-enabled="1"
+//         data-emit-metadata="0"
+//         data-input-position="top"
+//         data-theme="dark"
+//         data-lang="en"
+//         crossorigin="anonymous"
+//         async>
+// </script>
